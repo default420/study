bdecad03c9bc2ce42af35cd823cf0e04fb933068
t-tajiri <swallow.ava@gmail.com>
2019-01-12 22:54:54 +0900

    added database connection settings

--- env/db/run.sh
+++ env/db/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm --name demo_db -p 3306:3306 demo_db --skip-ssl
+docker run --rm --name demo_db -p 3306:3306 demo_db --default-authentication-plugin=mysql_native_password
