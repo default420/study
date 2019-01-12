9e974236f6c5db7552a6dec5df6cc4333ba56cfd
t-tajiri <swallow.ava@gmail.com>
2019-01-12 20:26:53 +0900

    refactoring script

--- demo-frontend/src/components/LineChart.vue
+++ demo-frontend/src/components/LineChart.vue
@@ -9,54 +9,55 @@ import Chart from 'chart.js'
 import ChartDataLabels from 'chartjs-plugin-datalabels'
 
 export default {
-  data () {
-    return {
-      chart: null,
-      data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-          label: 'Example Dataset',
-          borderColor: 'rgb(54, 162, 235)',
-          data: [
-            20,
-            30,
-            20,
-            15,
-            40,
-            25,
-            50
-          ],
-          datalabels: {
-            align: 'end'
-          },
-          fill: false
-        }]
-      },
-      options: {
-        responsive: true,
-        title: {
-          display: true,
-          text: 'Example Line Chart'
+  name: 'LineChart',
+
+  data: () => ({
+    chart: null,
+    data: {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [{
+        label: 'Example Dataset',
+        borderColor: 'rgb(54, 162, 235)',
+        data: [
+          20,
+          30,
+          20,
+          15,
+          40,
+          25,
+          50
+        ],
+        datalabels: {
+          align: 'end'
         },
-        scales: {
-          xAxes: [{
+        fill: false
+      }]
+    },
+    options: {
+      responsive: true,
+      title: {
+        display: true,
+        text: 'Example Line Chart'
+      },
+      scales: {
+        xAxes: [{
+          display: true,
+          scaleLabel: {
             display: true,
-            scaleLabel: {
-              display: true,
-              labelString: 'Month'
-            }
-          }],
-          yAxes: [{
+            labelString: 'Month'
+          }
+        }],
+        yAxes: [{
+          display: true,
+          scaleLabel: {
             display: true,
-            scaleLabel: {
-              display: true,
-              labelString: 'Value'
-            }
-          }]
-        }
+            labelString: 'Value'
+          }
+        }]
       }
     }
-  },
+  }),
+
   mounted () {
     let ctx = document.getElementById('sample')
     this.chart = new Chart(ctx, {
