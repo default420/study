6d4c37cc03a1ac0134001826c93e08dde388e00e
t-tajiri <swallow.ava@gmail.com>
2019-01-13 14:07:39 +0900

    added vuetify and apply it at components.

--- demo-frontend/src/App.vue
+++ demo-frontend/src/App.vue
@@ -1,24 +1,86 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">
-        Home
-      </router-link> |
-      <router-link to="/chart">
-        Chart
-      </router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-navigation-drawer
+      fixed
+      clipped
+      app
+      v-model="navBar"
+    >
+      <v-list
+        dense
+        class="pt-0"
+      >
+        <router-link to="/">
+          <v-list-tile>
+            <v-list-tile-action>
+              <v-icon>home</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Home</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </router-link>
+        <router-link to="/chart">
+          <v-list-tile>
+            <v-list-tile-action>
+              <v-icon>graphic_eq</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Chart</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </router-link>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar
+      dark
+      color="primary"
+      clipped-left
+      fixed
+      app
+    >
+      <v-toolbar-side-icon @click.stop="navBar = !navBar" />
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">
+          MATERIAL DESIGN
+        </span>
+      </v-toolbar-title>
+
+      <v-spacer />
+
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">
+          Latest Release
+        </span>
+      </v-btn>
+    </v-toolbar>
+    <v-content>
+      <v-container>
+        <v-fade-transition mode="out-in">
+          <router-view />
+        </v-fade-transition>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    navBar: null
+  })
 }
+</script>
+
+<style scoped>
+a {
+    text-decoration: none;
+  }
 </style>
